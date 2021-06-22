369d7f7870f73968475179b219dba1c7dd45573c
Cora-Jane Meade <corascompanycode@gmail.com>
2021-06-22 15:20:16 +0100

    Intial Commit

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Cognizant on 18/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
