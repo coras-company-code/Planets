596bf971e3908067d5d0974cb9c05a7f27c5eb7e
Cora-Jane Meade <corascompanycode@gmail.com>
2021-06-23 15:55:14 +0100

    Adding Model

---
+++ Planets/Model/PlanetManager.swift
@@ -0,0 +1,8 @@
+//
+//  PlanetManager.swift
+//  Planets
+//
+//  Created by M_931521 on 23/06/2021.
+//
+
+import Foundation
---
+++ Planets/Model/PlanetModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlanetModel.swift
+//  Planets
+//
+//  Created by M_931521 on 23/06/2021.
+//
+
+import Foundation
