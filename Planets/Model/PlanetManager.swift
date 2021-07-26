63ca0e6b0d6a005594bc1a196b252d129799d001
Cora-Jane Meade <corascompanycode@gmail.com>
2021-07-26 12:30:05 +0100

    Planet Manager to work, by removing fetch planets

--- Planets/Model/PlanetManager.swift
+++ Planets/Model/PlanetManager.swift
@@ -31,139 +31,48 @@ class PlanetManager {
         }
     }
     
-    func performRequest(urlString: String, completion: @escaping ([Any]?) -> Void) {
+    func performRequest(urlString: String, completion: @escaping ([Any]?) -> Void)  {
+        
         if let url = URL(string: urlString) {
+            
             let session = URLSession(configuration: .default)
+            
             let task = session.dataTask(with: url) { (data, response, error) in
+                
                 if error != nil {
                     print(error!)
                     completion(nil)
                     return
                 }
-                self.parseJSON(planetsData: data!) { (planets) in
-                    
+                
+                if let planets = self.parseJSON(planetsData: data!) {
                     completion(planets)
                 }
-                
-//                self.parseJSON(planetsData: data!) { (parsedPlanets) in //dont think i need a completion here as it does rely on the server
-//                    if let planets = parsedPlanets {
-//                        completion(planets)
-//                    }
-//                }
             }
             task.resume()
         }
     }
     
-    //func parseJSON(planetsData: Data) -> [PlanetModel]? {
-    func parseJSON(planetsData: Data, completion: @escaping ([PlanetModel]) -> Void) {
-        var planets: [PlanetModel] = []
-        var residents : [ResidentModel] = []
+    func parseJSON(planetsData: Data) -> [PlanetModel]? {
         
+        var planets: [PlanetModel] = []
         let decoder = JSONDecoder()
         do {
-            
             let decodedData = try decoder.decode(Results.self, from: planetsData)
             
+            
             for result in decodedData.results {
-                fetchResidents(urls: result.residentURLs) { (fetchedResident) in
-                    //print(fetchedResident!)
-                    residents.append(fetchedResident!)
-                    //var residents: [ResidentModel] = []
-                    //residents.append(resident)
-                    //print(residents)
-                    //var residents = []
-                    //residents.append(fetchedResident as! ResidentModel)
-                    //print(residents)
-                    let planet = PlanetModel(name: result.name, climate: result.climate, gravity: result.gravity, population: result.gravity, residentURLs: result.residentURLs, residentDetails: residents)
-                    planets.append(planet)
-                    completion(planets)
-                }
-               
-                
-                
-               
-                //let residents = [fetchResidents(urls: result.residentURLs)] //will this return a array or single
-               // print(residents)
-               
-                    
-                
+                let residents: [ResidentModel] = []
+                //let residents = fetchResidents(urls: result.residentURLs) //this will return an array
+                let planet = PlanetModel(name: result.name, climate: result.climate, gravity: result.gravity, population: result.gravity, residentURLs: result.residentURLs, residentDetails: residents)
+                planets.append(planet)
             }
         } catch {
-            print("Parsing Planet Error:\(error)")
+            print("Parsing Error:\(error)")
         }
-      //  return planets
-        
-        
-
+        return planets
     }
-    
-   func fetchResidents(urls: [String], completion: @escaping (ResidentModel?) -> Void) {
-    //func fetchResidents(urls: [String]) -> ResidentModel {
-        var resident: ResidentModel!
-            for url in urls {
-                let urlString = url
-                performResidentRequest(urlString: urlString){ (parsedResident) in
-                   completion(parsedResident)
-                    
-                   
-//                    if let resident = parsedResident! {
-//                    residents.append(resident)
-//                    }
-                    //why does the above work and not below
-//                    if let resident = parsedResident! {
-//                        print("Here")
-//                        print(resident)
-//                        residents.append(resident)
-//                    }
-                    
-//                    print("Here")
-//                    print(residents)
-                }
-                
-            }
-       // return resident
-        }
-
-    
-    
-  func performResidentRequest(urlString: String, completion: @escaping (ResidentModel?) -> Void) {
-        if let url = URL(string: urlString) {
-
-            let session = URLSession(configuration: .default)
-
-            let task = session.dataTask(with: url) { (data, response, error) in
-
-                if error != nil {
-                    print(error!)
-                    completion(nil)
-                    return
-                }
-             
-                if let resident = self.parseJSON(residentData: data!) {
-                    completion(resident)
-                }
-
-            }
-            task.resume()
-        }
-    }
-    
-    
-
-    func parseJSON(residentData: Data) -> ResidentModel? {
-        let decoder = JSONDecoder()
-        do {
-            let decodedData = try decoder.decode(ResidentModel.self, from: residentData)
-            let resident = ResidentModel(name: decodedData.name, height: decodedData.height, gender: decodedData.gender)
-            return resident
-        } catch {
-            print("Parsing Resident Error:\(error)")
-            return nil
-        }
-    }
-    
-    
+ 
     //MARK: - Model Manupulation Methods
 
     func loadItems(from filePath: URL?) {
---
+++ Planets/Model/ResidentManager.swift
@@ -0,0 +1,8 @@
+//
+//  ResidentManager.swift
+//  Planets
+//
+//  Created by M_931521 on 26/07/2021.
+//
+
+import Foundation
