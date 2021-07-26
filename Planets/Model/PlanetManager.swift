c31adf26a5599b8123596ea29cfee248f498aa70
Cora-Jane Meade <corascompanycode@gmail.com>
2021-07-26 17:33:09 +0100

    Extracted fetch residents out to resolve issue

--- Planets/Model/PlanetManager.swift
+++ Planets/Model/PlanetManager.swift
@@ -15,8 +15,7 @@ class PlanetManager {
     var delegate: PlanetManagerDelegate?
     let planetsURL = K.planetsURL
     let pageNumber = 1
-    //do i need both of these?:
-    var performedRequest = false
+    var performedRequest = false //Note to self: do i need both of these?
     var isLoading = true
    
     func fetchPlanets() {
@@ -26,25 +25,20 @@ class PlanetManager {
             self.isLoading = false
             if let planets = parsedDataArray as? [PlanetModel]  {
             self.delegate?.didUpdatePlanets(planets: planets)
-            print(planets)
+            print("Planets in fetch planets function: \(planets)")
             }
         }
     }
     
     func performRequest(urlString: String, completion: @escaping ([Any]?) -> Void)  {
-        
         if let url = URL(string: urlString) {
-            
             let session = URLSession(configuration: .default)
-            
             let task = session.dataTask(with: url) { (data, response, error) in
-                
                 if error != nil {
                     print(error!)
                     completion(nil)
                     return
                 }
-                
                 if let planets = self.parseJSON(planetsData: data!) {
                     completion(planets)
                 }
@@ -60,13 +54,20 @@ class PlanetManager {
         do {
             let decodedData = try decoder.decode(Results.self, from: planetsData)
             
+            //isolating one planet to test it is working:
+            let result = decodedData.results[0]
+            //let residents: [ResidentModel] = []
+            let residents = fetchResidents(urls: result.residentURLs) //this will return an array
+            let planet = PlanetModel(name: result.name, climate: result.climate, gravity: result.gravity, population: result.gravity, residentURLs: result.residentURLs, residentDetails: residents)
+            planets.append(planet)
             
-            for result in decodedData.results {
-                let residents: [ResidentModel] = []
-                //let residents = fetchResidents(urls: result.residentURLs) //this will return an array
-                let planet = PlanetModel(name: result.name, climate: result.climate, gravity: result.gravity, population: result.gravity, residentURLs: result.residentURLs, residentDetails: residents)
-                planets.append(planet)
-            }
+            //once working I will use this code:
+//            for result in decodedData.results {
+//                let residents: [ResidentModel] = []
+//                //let residents = fetchResidents(urls: result.residentURLs) //this will return an array
+//                let planet = PlanetModel(name: result.name, climate: result.climate, gravity: result.gravity, population: result.gravity, residentURLs: result.residentURLs, residentDetails: residents)
+//                planets.append(planet)
+//            }
         } catch {
             print("Parsing Error:\(error)")
         }
--- Planets/Model/ResidentManager.swift
+++ Planets/Model/ResidentManager.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+extension PlanetManager {
+    
+    func fetchResidents(urls: [String]) -> [ResidentModel] {
+        var residents: [ResidentModel] = []
+            for url in urls {
+                let urlString = url
+                performResidentRequest(urlString: urlString)  { (resident) in
+                    if resident != nil {
+                        residents.append(resident!) //print(resident!) residents are adding to the array
+                    }
+                }
+            }
+        print(residents) //how do i get whats above out to here? (i.e the apending residents)
+        return residents
+    }
+    
+    func performResidentRequest(urlString: String, completion: @escaping (ResidentModel?) -> ())  {
+
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    completion(nil)
+                }
+                if let resident = self.parseJSON(residentData: data!) {
+                    completion(resident)
+                }
+            }
+            task.resume()
+        }
+    }
+
+     func parseJSON(residentData: Data) -> ResidentModel? {
+         let decoder = JSONDecoder()
+         do {
+             let decodedData = try decoder.decode(ResidentModel.self, from: residentData)
+             let resident = ResidentModel(name: decodedData.name, height: decodedData.height, gender: decodedData.gender)
+             return resident
+         } catch {
+             print("Parsing Resident Error:\(error)")
+             return nil
+         }
+     }
+        
+}
